405fba8a9f57aa4fb6bb2a8a2544f581bffb3116
fleuveini <157180327+fleuveini@users.noreply.github.com>
2024-01-25 15:04:48 +0100

    Update and rename nombre 1er.py to nombre 1er v1.py

--- nombre 1er/nombre 1er v1.py
+++ nombre 1er/nombre 1er v1.py
@@ -1,16 +1,16 @@
 import json
 import time
 
-start=time.time()
-
 chemin="C:\\Users\\rapha\\OneDrive\\Bureau\\code\\nombre 1er\\nombre 1er.json"
 with open (chemin,"r") as f:
     listenb1er=json.load(f)
 
+start=time.time()
+
 nbtester=1
 diviseur=1
 test=0
-nb_facteurs=0
+nb_diviseur=0
 nb_de_nb1er=0
 
 if len(listenb1er)>2:
@@ -22,19 +22,17 @@ for loop in range(100):
         test=nbtester/diviseur
         diviseur+=1
         if test==int(test):
-            nb_facteurs+=1
-            
-    if nb_facteurs==2:
+            nb_diviseur+=1
+    
+    
+    if nb_diviseur==2:
         print(nbtester ,"est bien un nombre 1er")#nbtester
         listenb1er.append(nbtester)
         nb_de_nb1er+=1
-        
     nbtester+=1
     diviseur=1
-    nb_facteurs=0
-    
+    nb_diviseur=0
     with open(chemin, "w") as f:
-            json.dump(listenb1er, f, indent=4)
+        json.dump(listenb1er, f, indent=4)
 
-print(time.time()-start)
-
+print(time.time()-start,"sec")
