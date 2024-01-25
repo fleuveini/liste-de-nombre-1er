c54ebe3ec04f6c9f95368667007042129a937ee9
fleuveini <157180327+fleuveini@users.noreply.github.com>
2024-01-25 15:01:05 +0100

    Update nombre 1er.py

--- nombre 1er/nombre 1er.py
+++ nombre 1er/nombre 1er.py
@@ -1,4 +1,7 @@
 import json
+import time
+
+start=time.time()
 
 chemin="C:\\Users\\rapha\\OneDrive\\Bureau\\code\\nombre 1er\\nombre 1er.json"
 with open (chemin,"r") as f:
@@ -14,20 +17,24 @@ if len(listenb1er)>2:
         nbtester=max(listenb1er)
 
 for loop in range(100):
+    
     while diviseur <= nbtester:
         test=nbtester/diviseur
         diviseur+=1
         if test==int(test):
             nb_facteurs+=1
+            
     if nb_facteurs==2:
         print(nbtester ,"est bien un nombre 1er")#nbtester
         listenb1er.append(nbtester)
         nb_de_nb1er+=1
+        
     nbtester+=1
     diviseur=1
     nb_facteurs=0
+    
     with open(chemin, "w") as f:
             json.dump(listenb1er, f, indent=4)
 
-
+print(time.time()-start)
 
